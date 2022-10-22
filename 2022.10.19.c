3b4daf1a6bf41ed0ac56d9c6be3d356f7682b717
6Serendipity <1738328927@qq.com>
2022-10-22 09:16:35 +0800

    study2

--- 2022.10.19.c
+++ 2022.10.19.c
@@ -1,31 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main()
-{
-	int t, i, j;
-	int a[5];
-	for (i = 0;i <= 4;i++)
-	{
-		a[i] = 0;
-	}
-	i = 0;
-	while (i <= 4)
-	{
-		scanf("%d", &a[i]);
-		i++;
-	}
-	for (i = 0;i <= 3;i++)
-	{
-		for (j = 0;j <= 3 - i; j++)
-		{
-			if (a[j] < a[j + 1])
-			{
-				t = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = t;
-			}
-		}
-	}
-	printf("最小的数字为%d", a[4]);
-	return 0;
-}
+//int main()
+//{
+//	int t, i, j;
+//	int a[5];
+//	for (i = 0;i <= 4;i++)
+//	{
+//		a[i] = 0;
+//	}
+//	i = 0;
+//	while (i <= 4)
+//	{
+//		scanf("%d", &a[i]);
+//		i++;
+//	}
+//	for (i = 0;i <= 3;i++)
+//	{
+//		for (j = 0;j <= 3 - i; j++)
+//		{
+//			if (a[j] < a[j + 1])
+//			{
+//				t = a[j];
+//				a[j] = a[j + 1];
+//				a[j + 1] = t;
+//			}
+//		}
+//	}
+//	printf("最小的数字为%d", a[4]);
+//	return 0;
+//}
---
+++ 2022.10.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "_2022.h"
+#if __has_include("_2022.g.cpp")
+#include "_2022.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::github::implementation
+{
+    _2022::_2022()
+    {
+        InitializeComponent();
+    }
+
+    int32_t _2022::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void _2022::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void _2022::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
